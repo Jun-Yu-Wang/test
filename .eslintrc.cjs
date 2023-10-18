0b519f3a3a727b73106a98fc496da9fd64ec7d5b
jun.wang <jun.wang@sheng-sen.com>
2023-10-18 20:55:09 +0800

    第一次建立

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,75 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
+  extends: [
+    './.eslintrc-auto-import.json',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:prettier/recommended',
   ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+  rules: {
+    'vue/valid-template-root': 'error',
+    'vue/no-multiple-template-root': 'off',
+    'vue/multi-word-component-names': 'off',
+    'no-unused-vars': 'off',
+    'prettier/prettier': 'off', // 解决replace with
+
+    'no-console': 'off',
+    'no-continue': 'off',
+    'no-restricted-syntax': 'off',
+    'no-plusplus': 'off',
+    'no-param-reassign': 'off',
+    'no-shadow': 'off',
+    'guard-for-in': 'off',
+
+    // "import/extensions": "off",
+    // "import/no-unresolved": "off",
+    // "import/no-extraneous-dependencies": "off",
+    // "import/prefer-default-export": "off",
+    // "import/first": "off", // https://github.com/vuejs/vue-eslint-parser/issues/58
+    // "vue/first-attribute-linebreak": 0
+  },
+  // parserOptions: {
+  //   ecmaVersion: "latest"
+  // },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        'vue/require-default-prop': 0,
+        'vue/multi-word-component-names': 0,
+        'vue/no-reserved-props': 0,
+        // 'vue/no-v-html': 2,
+      },
+    },
+    // {
+    //   files: ["*.ts", "*.tsx"], // https://github.com/typescript-eslint eslint-recommended
+    //   rules: {
+    //     "constructor-super": "off", // ts(2335) & ts(2377)
+    //     "getter-return": "off", // ts(2378)
+    //     "no-const-assign": "off", // ts(2588)
+    //     "no-dupe-args": "off", // ts(2300)
+    //     "no-dupe-class-members": "off", // ts(2393) & ts(2300)
+    //     "no-dupe-keys": "off", // ts(1117)
+    //     "no-func-assign": "off", // ts(2539)
+    //     "no-import-assign": "off", // ts(2539) & ts(2540)
+    //     "no-new-symbol": "off", // ts(2588)
+    //     "no-obj-calls": "off", // ts(2349)
+    //     "no-redeclare": "off", // ts(2451)
+    //     "no-setter-return": "off", // ts(2408)
+    //     "no-this-before-super": "off", // ts(2376)
+    //     "no-undef": "off", // ts(2304)
+    //     "no-unreachable": "off", // ts(7027)
+    //     "no-unsafe-negation": "off", // ts(2365) & ts(2360) & ts(2358)
+    //     "no-var": "error", // ts transpiles let/const to var, so no need for vars any more
+    //     "prefer-const": "error", // ts provides better types with const
+    //     "prefer-rest-params": "error", // ts provides better types with rest args over arguments
+    //     "prefer-spread": "error", // ts transpiles spread to apply, so no need for manual apply
+    //     "valid-typeof": "off" // ts(2367)
+    //   }
+    // }
+  ],
+};
---
+++ src/layout/Index.vue
@@ -0,0 +1,175 @@
+<template>
+  <table width="850" border="0" align="center" cellpadding="0" cellspacing="0">
+    <tbody>
+      <tr>
+        <td>
+          <table width="850" border="0" cellspacing="0" cellpadding="0">
+            <tbody>
+              <tr>
+                <td width="350">
+                  <img src="./表格製作1_files/style02_02.gif" width="301" height="22" />
+                </td>
+                <td bgcolor="#FFFFFF">
+                  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                      <tr>
+                        <td background="./表格製作1_files/16_03.gif">
+                          <img src="./表格製作1_files/16_03.gif" width="3" height="3" />
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+                <td width="365">
+                  <div align="left"></div>
+                  <table border="0" cellpadding="0" cellspacing="0">
+                    <tbody>
+                      <tr>
+                        <td bgcolor="#FFFFFF" class="a02">
+                          ．工程類&nbsp;|．總務、公關&nbsp;|．行銷&nbsp;|．Web應用&nbsp;|．其他
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+      <tr>
+        <td><img src="./表格製作1_files/style02_05.gif" width="850" height="15" /></td>
+      </tr>
+      <tr>
+        <td>
+          <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tbody>
+              <tr>
+                <td width="16">
+                  <img src="./表格製作1_files/style02_06.gif" width="16" height="136" />
+                </td>
+                <td background="./表格製作1_files/15_01.gif">
+                  <object
+                    classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
+                    codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0"
+                    width="818"
+                    height="136"
+                  >
+                    <param name="movie" value="neter/banner.swf" />
+                    <param name="quality" value="high" />
+                    <param name="wmode" value="transparent" />
+                    <embed
+                      src="neter/banner.swf"
+                      width="818"
+                      height="136"
+                      quality="high"
+                      pluginspage="http://www.macromedia.com/go/getflashplayer"
+                      type="application/x-shockwave-flash"
+                      wmode="transparent"
+                    />
+                  </object>
+                </td>
+                <td width="16">
+                  <img src="./表格製作1_files/style02_09.gif" width="16" height="136" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+      <tr>
+        <td><img src="./表格製作1_files/style02_10.gif" width="850" height="12" /></td>
+      </tr>
+      <tr>
+        <td>
+          <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tbody>
+              <tr>
+                <td width="16">
+                  <img src="./表格製作1_files/style02_11.gif" width="16" height="28" />
+                </td>
+                <td background="./表格製作1_files/style02_12-g.gif">
+                  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tbody>
+                      <tr>
+                        <td width="10">&nbsp;</td>
+                        <td>
+                          <p class="13px_word">
+                            <a href="https://dam.cht.com.tw/CHT_DAM/o2.jsp" class="a01">資料上載</a>
+                            <font color="#FFFFFF"
+                              >&nbsp;|&nbsp;<a
+                                href="https://dam.cht.com.tw/CHT_DAM/v1.jsp"
+                                class="a01"
+                                >資料．搜尋 / 修改</a
+                              >&nbsp;|&nbsp;<a
+                                href="https://dam.cht.com.tw/CHT_DAM/table.jsp"
+                                class="a01"
+                                >表格製作</a
+                              >&nbsp;|&nbsp;<a
+                                href="https://dam.cht.com.tw/CHT_DAM/search.jsp"
+                                class="a01"
+                                >未結案表格修改</a
+                              >&nbsp;|&nbsp;<a
+                                href="https://dam.cht.com.tw/CHT_DAM/search2.jsp"
+                                class="a01"
+                                >已結案表格查詢</a
+                              >&nbsp;|&nbsp;<a
+                                href="https://dam.cht.com.tw/CHT_DAM/work.jsp"
+                                class="a01"
+                                >待處理案件</a
+                              >&nbsp;|&nbsp;<a
+                                href="https://dam.cht.com.tw/CHT_DAM/note2.jsp"
+                                class="a01"
+                                >系統資訊</a
+                              >&nbsp;|&nbsp;<a
+                                href="https://dam.cht.com.tw/CHT_DAM/message.jsp"
+                                class="a01"
+                                >公告消息</a
+                              ></font
+                            >
+                          </p>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+                <td width="16">
+                  <img src="./表格製作1_files/style02_13.gif" width="16" height="28" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+      <tr>
+        <td><img src="./表格製作1_files/style02_14.gif" width="850" height="14" /></td>
+      </tr>
+      <tr>
+        <td></td>
+      </tr>
+      <tr>
+        <td><img src="./表格製作1_files/style02_19-g.gif" width="850" height="17" /></td>
+      </tr>
+      <tr>
+        <td>
+          <table width="100%" border="0" cellspacing="0" cellpadding="10">
+            <tbody>
+              <tr>
+                <td>
+                  <div align="center">
+                    COPYRIGHT &#169; 2005 CHUNGHWA TELECOM CO., LTD.<br />
+                    版權所有：中華電信股份有限公司
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup></script>
+
+<style></style>
